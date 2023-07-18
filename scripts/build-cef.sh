e3c646ba8ada5b329f87a235afddcde9a29522ec
Mark Wieder <mwieder@ahsoftware.net>
2023-07-18 16:00:55 -0700

    fixed cef download address

--- scripts/build-cef.sh
+++ scripts/build-cef.sh
@@ -4,13 +4,20 @@ source "${BASEDIR}/scripts/platform.inc"
 source "${BASEDIR}/scripts/lib_versions.inc"
 source "${BASEDIR}/scripts/util.inc"
 
+# 2023.07.18 currently ${CEF_BUILDVERSION} is 5672
+
+CEF_BUILDVERSION="5672"
+CEF_URL="https://bitbucket.org/chromiumembedded/cef/get/"
+
 # Grab the source for the library
 if [ "${ARCH}" == "x86" ] ; then
-   CEF_DST="cef_binary_${CEF_VERSION}+${CEF_BUILDREVISION}+chromium-${CEFChromium_VERSION}_${PLATFORM}32"
-   CEF_SRC="cef_binary_${CEF_VERSION}%2B${CEF_BUILDREVISION}%2Bchromium-${CEFChromium_VERSION}_${PLATFORM}32.tar.bz2"
+#   CEF_DST="cef_binary_${CEF_VERSION}+${CEF_BUILDREVISION}+chromium-${CEFChromium_VERSION}_${PLATFORM}32"
+#   CEF_SRC="cef_binary_${CEF_VERSION}%2B${CEF_BUILDREVISION}%2Bchromium-${CEFChromium_VERSION}_${PLATFORM}32.tar.bz2"
+	CEF_DST="../fetched/CEF-${CEF_BUILDVERSION}_${PLATFORM}32"
 elif [ "${ARCH}" == "x86_64" ] ; then
-   CEF_DST="cef_binary_${CEF_VERSION}+${CEF_BUILDREVISION}+chromium-${CEFChromium_VERSION}_${PLATFORM}64"
-   CEF_SRC="cef_binary_${CEF_VERSION}%2B${CEF_BUILDREVISION}%2Bchromium-${CEFChromium_VERSION}_${PLATFORM}64.tar.bz2"
+#   CEF_DST="cef_binary_${CEF_VERSION}+${CEF_BUILDREVISION}+chromium-${CEFChromium_VERSION}_${PLATFORM}64"
+#   CEF_SRC="cef_binary_${CEF_VERSION}%2B${CEF_BUILDREVISION}%2Bchromium-${CEFChromium_VERSION}_${PLATFORM}64.tar.bz2"
+	CEF_DST="../fetched/CEF-${CEF_BUILDVERSION}_${PLATFORM}64"
 else 
   echo "No binaries available for arch"
 fi
@@ -21,9 +28,10 @@ cd "${BUILDDIR}"
 if [ ! -d "$CEF_DST" ] ; then
 	if [ ! -e "$CEF_TGZ" ] ; then
 		echo "Fetching CEF source"
-		fetchUrl "http://opensource.spotify.com/cefbuilds/${CEF_SRC}" "${CEF_TGZ}"
+#		fetchUrl "http://opensource.spotify.com/cefbuilds/${CEF_SRC}" "${CEF_TGZ}"
+		fetchUrl "${CEF_URL}${CEF_BUILDVERSION}" "${CEF_TGZ}"
 		if [ $? != 0 ] ; then
-			echo "downloading http://opensource.spotify.com/cefbuilds/${CEF_SRC} failed"
+			echo "downloading ${CEF_URL}${CEF_BUILDVERSION} failed"
 			if [ -e "${CEF_TGZ}" ] ; then 
 				rm ${CEF_TGZ} 
 			fi
